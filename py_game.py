83e787bd1569193aa932d636b467dc96a11cf669
okamotomasatosi <okamotomasatosi@gmail.com>
2019-08-22 16:36:42 +0900

    変数名などをキャメルからスネークに修正

--- py_game.py
+++ py_game.py
@@ -8,13 +8,15 @@ import pygame
 from pygame.locals import *
 
 SCREEN_SET = Rect(0, 0, 800, 700)
+
+
 ###################################
 #
 # 　ここからメイン
 #
 ###################################
 def main():
-    #変数の初期化
+    # 変数の初期化
     x1_pt = 150
     y1_pt = 0
     score = 0
@@ -29,86 +31,89 @@ def main():
              [1, 0, 0, 0, 0, 0, 1],
              [1, 1, 1, 1, 1, 1, 1]]
 
-    pygame.init()    # Pygameの初期化
+    pygame.init()  # Pygameの初期化
     screen = pygame.display.set_mode((800, 700))  # 画面サイズを指定して画面を生成
     pygame.display.set_caption("GAME")  # タイトルバーに表示する文字
-    screen.fill((0, 0, 0))      # 画面を黒色に塗りつぶし
-    list0 = copy.deepcopy(list1)    # ディープコピー
-    screen.fill((0, 0, 0))      # ウィンドウの背景色
+    screen.fill((0, 0, 0))  # 画面を黒色に塗りつぶし
+    list0 = copy.deepcopy(list1)  # ディープコピー
+    screen.fill((0, 0, 0))  # ウィンドウの背景色
     face_img = pygame.image.load("./img/mas_face.jpg")
     put_img(screen, face_img, 400, 100)  # 画面に顔を置く
 
-    #初期の猫の色を決める
-    catColor = random.randint(2,7)
+    # 初期の猫の色を決める
+    cat_color = random.randint(2, 7)
 
-    #ココからメインのループ
+    # ココからメインのループ
     while (1):
-        #一番上まで埋まったかチェック
-        retCollision: bool = chekCollision(int(x1_pt / 50), int(y1_pt / 50), list0)
-        if retCollision:
+        # 一番上まで埋まったかチェック
+        ret_collision: bool = chek_collision(int(x1_pt / 50), int(y1_pt / 50), list0)
+        if ret_collision:
             # 一番上まで埋まってたので、暫定的に枠内をクリア
             list0 = copy.deepcopy(list1)  # ディープコピー
-        put_frame(screen, list0)    # 画面に枠を置く
-        time.sleep(0.2)             # 速度調整のsleep
+        put_frame(screen, list0)  # 画面に枠を置く
+        time.sleep(0.2)  # 速度調整のsleep
 
         # キー入力など、イベント処理
-        x1_pt,y1_pt = event_proc(x1_pt,y1_pt)
+        x1_pt, y1_pt = event_proc(x1_pt, y1_pt)
 
-        y1_pt = y1_pt + 50        #ブロック落下
+        y1_pt = y1_pt + 50  # ブロック落下
 
-        #ブロック当たりチェック
-        retCollision:bool = chekCollision(int(x1_pt/50), int(y1_pt/50), list0)
-        if retCollision:
-            y1_pt = y1_pt - 50      # 落下できなかったので戻す
-            #ブロックの固定
-            list0 = setBlockList(int(x1_pt/50), int(y1_pt/50), catColor, list0)
-            #並びブロックのチェックと削除
-            list0,score = chekDeleteCat(int(x1_pt/50), int(y1_pt/50), list0, score)
-            catColor = random.randint(2, 7)     # 次の猫色を決める
-            y1_pt = 0                           # ぶつかったのでy座標をゼロに戻す
-            put_frame(screen, list0)            # 画面に枠を置く
+        # ブロック当たりチェック
+        ret_collision: bool = chek_collision(int(x1_pt / 50), int(y1_pt / 50), list0)
+        if ret_collision:
+            y1_pt = y1_pt - 50  # 落下できなかったので戻す
+            # ブロックの固定
+            list0 = set_block_list(int(x1_pt / 50), int(y1_pt / 50), cat_color, list0)
+            # 並びブロックのチェックと削除
+            list0, score = chek_delete_cat(int(x1_pt / 50), int(y1_pt / 50), list0, score)
+            cat_color = random.randint(2, 7)  # 次の猫色を決める
+            y1_pt = 0  # ぶつかったのでy座標をゼロに戻す
+            put_frame(screen, list0)  # 画面に枠を置く
 
         # 閾値超えたら戻るように
         if y1_pt > 400:
             y1_pt = 0
 
-        #落ちてくるブロック（猫）を画面に置く
-        putDropCat(screen, catColor, x1_pt, y1_pt)
+        # 落ちてくるブロック（猫）を画面に置く
+        put_drop_cat(screen, cat_color, x1_pt, y1_pt)
 
         ##pygame.display.flip()  # display Surface全体を更新して画面に描写します。
-        pygame.display.update()     # スクリーンの一部分のみを更新します。この命令はソフトウェア側での表示処理に最適化されています。
+        pygame.display.update()  # スクリーンの一部分のみを更新します。この命令はソフトウェア側での表示処理に最適化されています。
 
-        putScore(screen,score)      # スコアの表示
+        put_score(screen, score)  # スコアの表示
+
 
 ## スコアの表示
-def putScore(screen,score):
+def put_score(screen, score):
     font = pygame.font.Font(None, 40)  # フォントの設定(55px)
-    put_img(screen, getBlockImg(0), 400, 50)  # スコア表示部分に黒ブロックを置いて消す
-    put_img(screen, getBlockImg(0), 450, 50)  # スコア表示部分に黒ブロックを置いて消す
-    put_img(screen, getBlockImg(0), 500, 50)  # スコア表示部分に黒ブロックを置いて消す
-    screen.blit(font.render(str(score), True, (255,255,0)), [400, 50])# 文字列の表示位置
+    put_img(screen, get_block_img(0), 400, 50)  # スコア表示部分に黒ブロックを置いて消す
+    put_img(screen, get_block_img(0), 450, 50)  # スコア表示部分に黒ブロックを置いて消す
+    put_img(screen, get_block_img(0), 500, 50)  # スコア表示部分に黒ブロックを置いて消す
+    screen.blit(font.render(str(score), True, (255, 255, 0)), [400, 50])  # 文字列の表示位置
+
 
 ## ブロックの削除対象チェックと削除
-def chekDeleteCat(x, y, list0: list, score) :
-    if y>6:
+def chek_delete_cat(x, y, list0: list, score):
+    if y > 6:
         # 底から2段目以下ならば3個並んでいないので即返る
-        return list0,score
-    elif y<2:
+        return list0, score
+    elif y < 2:
         # 天から2段目以上ならば3個並んでいないので即返る
-        return list0,score
+        return list0, score
     cat0 = list0[y][x]
-    cat1 = list0[y+1][x]
-    cat2 = list0[y+2][x]
+    cat1 = list0[y + 1][x]
+    cat2 = list0[y + 2][x]
     if (cat0 == cat1) and (cat1 == cat2):
         # 3個並んでる
-        list0[y][x]=0
-        list0[y + 1][x]=0
-        list0[y + 2][x]=0
+        list0[y][x] = 0
+        list0[y + 1][x] = 0
+        list0[y + 2][x] = 0
         score = score + 3
-    return list0,score
+    return list0, score
+
 
 ## ブロックの当たり判定　※簡易なので下しか見ていない。本来は横も見る
-def chekCollision(x, y, list0:list) -> bool:
+def chek_collision(x, y, list0: list) -> bool:
     block = list0[y][x]
     print('block=', block)
     if block == 0:
@@ -116,68 +121,73 @@ def chekCollision(x, y, list0:list) -> bool:
     else:
         return True
 
+
 ## 着地したブロックを変数に書き込む
-def setBlockList(x, y, cat_col,list0: list)-> list:
+def set_block_list(x, y, cat_col, list0: list) -> list:
     list0[y][x] = cat_col
     return list0
 
+
 ## キー入力など、イベント処理
 ##　矢印キーでブロックの座標他仕込みも行う
-def event_proc(x1_pt:int, y1_pt:int) -> object:
-        # イベント処理
-        for event in pygame.event.get():
-            if event.type == QUIT:  # 閉じるボタンが押されたら終了
-                pygame.quit()  # Pygameの終了(画面閉じられる)
-                sys.exit()
-            # キー操作(追加したとこ)
-            elif event.type == KEYDOWN:
-                print("event.type=" + str(event.type))
-                print("event.key=" + str(event.key))
-                if event.key == K_LEFT:
-                    print("←")
-                    x1_pt -= 50
-                elif event.key == K_RIGHT:
-                    print("→")
-                    x1_pt += 50
-                #elif event.key == K_UP:
-                    #print("↑")
-                    #y1_pt -= 50
-                #elif event.key == K_DOWN:
-                    #print("↓")
-                    #y1_pt += 50
-        if x1_pt < 50:
-            x1_pt = 50
-        if x1_pt > 250:
-            x1_pt = 250
-        #　座標のみ返す
-        return x1_pt,y1_pt
+def event_proc(x1_pt: int, y1_pt: int) -> object:
+    # イベント処理
+    for event in pygame.event.get():
+        if event.type == QUIT:  # 閉じるボタンが押されたら終了
+            pygame.quit()  # Pygameの終了(画面閉じられる)
+            sys.exit()
+        # キー操作(追加したとこ)
+        elif event.type == KEYDOWN:
+            print("event.type=" + str(event.type))
+            print("event.key=" + str(event.key))
+            if event.key == K_LEFT:
+                print("←")
+                x1_pt -= 50
+            elif event.key == K_RIGHT:
+                print("→")
+                x1_pt += 50
+            # elif event.key == K_UP:
+            # print("↑")
+            # y1_pt -= 50
+            # elif event.key == K_DOWN:
+            # print("↓")
+            # y1_pt += 50
+    if x1_pt < 50:
+        x1_pt = 50
+    if x1_pt > 250:
+        x1_pt = 250
+    # 　座標のみ返す
+    return x1_pt, y1_pt
+
 
 ##　指定のブロック絵柄（猫など）をイメージオブジェクトに入れて返す
-def getBlockImg(catColor:int):
-    if catColor == 0:
-        retImg = pygame.image.load("./img/blank.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 1:
-        retImg = pygame.image.load("./img/block.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 2:
-        retImg = pygame.image.load("./img/neko0.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 3:
-        retImg = pygame.image.load("./img/neko1.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 4:
-        retImg = pygame.image.load("./img/neko2.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 5:
-        retImg = pygame.image.load("./img/neko3.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 6:
-        retImg = pygame.image.load("./img/neko4.png")  # 画像を読み込む(今回追加したとこ)
-    elif catColor == 7:
-        retImg = pygame.image.load("./img/neko5.png")  # 画像を読み込む(今回追加したとこ)
+def get_block_img(cat_color: int):
+    if cat_color == 0:
+        ret_img = pygame.image.load("./img/blank.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 1:
+        ret_img = pygame.image.load("./img/block.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 2:
+        ret_img = pygame.image.load("./img/neko0.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 3:
+        ret_img = pygame.image.load("./img/neko1.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 4:
+        ret_img = pygame.image.load("./img/neko2.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 5:
+        ret_img = pygame.image.load("./img/neko3.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 6:
+        ret_img = pygame.image.load("./img/neko4.png")  # 画像を読み込む(今回追加したとこ)
+    elif cat_color == 7:
+        ret_img = pygame.image.load("./img/neko5.png")  # 画像を読み込む(今回追加したとこ)
     else:
-        retImg = pygame.image.load("./img/block.png")  # 画像を読み込む(今回追加したとこ)
-    return retImg
+        ret_img = pygame.image.load("./img/block.png")  # 画像を読み込む(今回追加したとこ)
+    return ret_img
+
 
 ##　落ちてくるブロック（猫）を画面に置く
-def putDropCat(screen, catColor:int, x1_pt, y1_pt):
-    dropCatImg = getBlockImg(catColor)
-    screen.blit(dropCatImg, (x1_pt, y1_pt))
+def put_drop_cat(screen, cat_color: int, x1_pt, y1_pt):
+    drop_cat_img = get_block_img(cat_color)
+    screen.blit(drop_cat_img, (x1_pt, y1_pt))
+
 
 ##フレーム内を描き直す
 def put_frame(screen, list):
@@ -187,18 +197,21 @@ def put_frame(screen, list):
         x_idx = 0
         for val2 in val1:
             # idBlokck = list2[0][index2]
-            idBlokck = int(val2)
-            put_block(screen, getBlockImg(idBlokck), x_idx, y_idx)
+            id_blokck = int(val2)
+            put_block(screen, get_block_img(id_blokck), x_idx, y_idx)
             x_idx += 1
         y_idx += 1
+
 
 ## 絵を表示する
 def put_img(screen, img_obj, x, y):
     screen.blit(img_obj, (x, y, 50, 50))  # 絵を画面に貼り付ける
 
+
 ## ブロックを画面に置く ※50ドット単位
 def put_block(screen, img_obj, x, y):
     screen.blit(img_obj, (50 * x, 50 * y, 50, 50))  # 絵を画面に貼り付ける
+
 
 ##画面に資格を書く
 def put_rect(screen, x, y):
@@ -206,6 +219,7 @@ def put_rect(screen, x, y):
     rect = ((50 * x), (50 * y), 50, 50)
     pygame.draw.rect(screen, RED, rect)
     # screen.blit(font.render( in_chr, True, (255, 255, 0)), [(50 * x_idx), (40 * y_idx)])  # 文字列の表示位置
+
 
 ##　既定　これが無いと動かない    #############################
 if __name__ == "__main__":
